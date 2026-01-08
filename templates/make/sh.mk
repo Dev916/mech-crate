423944890b1e23775717772c13c85add3b43112f
web-mech <webmech@gmail.com>
2026-01-08 00:30:08 -0500

    chore(make): add descriptions for bash and sh targets in makefile

--- templates/make/sh.mk
+++ templates/make/sh.mk
@@ -1,12 +1,10 @@
 # Shell commands
 .PHONY: sh bash _sh
 
-# Shell into service (alias for sh, s=[service])
-bash:
+bash: ## Shell into service (alias for sh, s=[service])
 	@$(MAKE) _sh service=$(call get_service)
 
-# Shell into a running service (s=[service])
-sh:
+sh: ## Shell into a running service (s=[service])
 	@$(MAKE) _sh service=$(call get_service)
 
 _sh:
