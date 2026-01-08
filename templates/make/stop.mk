b60a7b76ba1f99f8172fec88dbc8b82774e0f7eb
web-mech <webmech@gmail.com>
2026-01-08 00:30:10 -0500

    fix(make): update stop command comment formatting

--- templates/make/stop.mk
+++ templates/make/stop.mk
@@ -1,8 +1,7 @@
 # Stop commands
 .PHONY: stop _stop
 
-# Stop services without removing (s=[service] for specific)
-stop:
+stop: ## Stop services without removing (s=[service] for specific)
 	@$(MAKE) _stop service=$(call get_service_optional)
 
 _stop:
