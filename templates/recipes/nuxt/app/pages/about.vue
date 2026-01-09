410d6edfe360b85f4008364e348b4916d40e5286
web-mech <webmech@gmail.com>
2026-01-08 23:14:04 -0500

    feat(nuxt): add about page with application features list

---
+++ templates/recipes/nuxt/app/pages/about.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="prose max-w-none">
+    <h1>About</h1>
+    <p>This is a Nuxt 3 application created with MechCrate.</p>
+    
+    <h2>Features</h2>
+    <ul>
+      <li>Nuxt 3 with SSR</li>
+      <li>Tailwind CSS + DaisyUI</li>
+      <li>TypeScript</li>
+      <li>Docker containerization</li>
+      <li>Traefik routing</li>
+    </ul>
+  </div>
+</template>
